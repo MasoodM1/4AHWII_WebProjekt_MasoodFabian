4e5f2cfe40e03037fc5aa5767c57123411528b29
Masood <mmuradi@tsn.at>
2024-03-21 09:26:34 +0100

    Projektdateien hinzufügen.

---
+++ 4AHWII_WebProjekt_MasoodFabian/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _4AHWII_WebProjekt_MasoodFabian.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
