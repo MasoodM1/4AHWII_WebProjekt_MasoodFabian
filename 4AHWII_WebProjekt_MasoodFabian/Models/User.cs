d8fbbc3f3ac8118b5910dbcecc4cf2a9ee1dc63c
Fabix1204 <fabialeitner@tsn.at>
2024-04-18 08:55:57 +0200

    added user

---
+++ 4AHWII_WebProjekt_MasoodFabian/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace _4AHWII_WebProjekt_MasoodFabian.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public DateTime Geburtsdatum { get; set; }
+
+    }
+}
