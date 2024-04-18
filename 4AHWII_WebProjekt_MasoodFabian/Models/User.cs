16f36fafbcb194dde083b4da565b97c1211f77d3
Fabix1204 <fabialeitner@tsn.at>
2024-04-18 09:20:48 +0200

    added user and db

--- 4AHWII_WebProjekt_MasoodFabian/Models/User.cs
+++ 4AHWII_WebProjekt_MasoodFabian/Models/User.cs
@@ -3,9 +3,36 @@
     public class User
     {
         public int Id { get; set; }
+        public string Username { get; set; }
+        public string Passwort { get; set; }
+        public string Email { get; set; }
         public string Vorname { get; set; }
         public string Nachname { get; set; }
+        public string Telefon { get; set; }
+        public string Adresse { get; set; }
         public DateTime Geburtsdatum { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool Newsletter { get; set; }
 
+        public User(int id, string username, string password, string email, string vorname, string nachname, string phoneNumber, string address, DateTime geburtsdatum, bool isAdmin, bool newsletter)
+        {
+            Id = id;
+            Username = username;
+            Passwort = password;
+            Email = email;
+            Vorname = vorname;
+            Nachname = nachname;
+            Telefon = phoneNumber;
+            Adresse = address;
+            Geburtsdatum = geburtsdatum;
+            IsAdmin = isAdmin;
+            Newsletter = newsletter;
+        }
+
+        public string ToString()
+        {
+            return "Username:" + Username + "Email: " + Email + "Vorname: " + Vorname + "Nachname: " + Nachname
+                + "Phone: " + Telefon + "Geburtsdatum: " + Geburtsdatum;
+        }
     }
 }
