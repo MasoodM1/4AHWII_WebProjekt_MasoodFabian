767676277aad786f8038422102d3fa72f42df3f9
Fabix1204 <fabialeitner@tsn.at>
2024-04-25 09:16:52 +0200

    DatabaseUpdate

--- 4AHWII_WebProjekt_MasoodFabian/Migrations/20240425071406_Users_2.Designer.cs
+++ 4AHWII_WebProjekt_MasoodFabian/Migrations/20240425071406_Users_2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using _4AHWII_WebProjekt_MasoodFabian.Models.DB;
 
@@ -11,9 +12,11 @@ using _4AHWII_WebProjekt_MasoodFabian.Models.DB;
 namespace _4AHWII_WebProjekt_MasoodFabian.Migrations
 {
     [DbContext(typeof(DbManager))]
-    partial class DbManagerModelSnapshot : ModelSnapshot
+    [Migration("20240425071406_Users_2")]
+    partial class Users_2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
