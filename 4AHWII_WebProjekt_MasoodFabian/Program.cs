c2b1d5a31830b7811e4e9de859f25b56d9c5ab78
Masood <mmuradi@tsn.at>
2024-04-25 09:28:54 +0200

    Register_Login

--- 4AHWII_WebProjekt_MasoodFabian/Program.cs
+++ 4AHWII_WebProjekt_MasoodFabian/Program.cs
@@ -28,7 +28,7 @@ namespace _4AHWII_WebProjekt_MasoodFabian
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Login}/{id?}");
 
             app.Run();
         }
