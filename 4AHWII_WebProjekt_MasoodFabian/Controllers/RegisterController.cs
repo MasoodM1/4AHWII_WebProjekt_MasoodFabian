a1f55f13fc556bbb00ce91fba23391b9e40e093a
Fabix1204 <fabialeitner@tsn.at>
2024-05-04 00:31:13 +0200

    added working register page

--- 4AHWII_WebProjekt_MasoodFabian/Controllers/RegisterController.cs
+++ 4AHWII_WebProjekt_MasoodFabian/Controllers/RegisterController.cs
@@ -1,21 +1,22 @@
-﻿using _4AHWII_WebProjekt_MasoodFabian.Models.DB;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using _4AHWII_WebProjekt_MasoodFabian.Models;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Linq;
+using _4AHWII_WebProjekt_MasoodFabian.Models.DB;
 
 namespace _4AHWII_WebProjekt_MasoodFabian.Controllers
 {
     public class RegisterController : Controller
     {
         private readonly DbManager _dbManager;
-        private readonly PasswordHasher<User> _passwordHasher;
 
-        public RegisterController(DbManager dbManager, PasswordHasher<User> passwordHasher)
+        public RegisterController(DbManager dbManager)
         {
             _dbManager = dbManager;
-            _passwordHasher = passwordHasher;
         }
+
         public IActionResult Index()
         {
             return View();
@@ -46,39 +47,35 @@ namespace _4AHWII_WebProjekt_MasoodFabian.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(User user)
         {
-            if(user == null)
+            if (user == null)
             {
                 return View();
             }
-            if(user.Username == null)
+            if (user.Username == null)
             {
                 ModelState.AddModelError("Username", "Bitte geben Sie einen Username ein!");
             }
-            if(user.Passwort == null || user.Passwort.Trim().Length < 8)
+            if (user.Passwort == null || user.Passwort.Trim().Length < 8 ||
+               !user.Passwort.Any(char.IsDigit) || !user.Passwort.Any(isSpecialCharacter) ||
+               !user.Passwort.Any(char.IsLower) || !user.Passwort.Any(char.IsUpper))
             {
-                ModelState.AddModelError("Passwort", "Das Passwort muss mindestens 8 Zeichen lang sein!");
+                ModelState.AddModelError("Passwort", "Das Passwort muss mindestens 8 Zeichen lang sein und mindestens eine Zahl, ein Großbuchstabe, ein Kleinbuchstabe und ein Sonderzeichen enthalten!");
             }
-            if(user.Passwort == null || !user.Passwort.Any(char.IsDigit))
-            {
-                ModelState.AddModelError("Passwort", "Das Passwort muss mindestens eine Zahl beinhalten!");
-            }
-            if(user.Passwort == null || !user.Passwort.Any(isSpecialCharacter)) {
-                ModelState.AddModelError("Passwort", "Das Passwort muss mindestens ein Sonderzeichen beinhalten!");
-            }
-            if(user.Email == null || !IsValidEmail(user.Email))
+            if (user.Email == null || !IsValidEmail(user.Email))
             {
                 ModelState.AddModelError("Email", "Bitte geben Sie eine gültige Email-Adresse ein!");
             }
-            if(user.Geburtsdatum > DateTime.Now)
+            if (user.Geburtsdatum > DateTime.Now)
             {
                 ModelState.AddModelError("Geburtsdatum", "Bitte geben Sie ein gültiges Geburtsdatum ein!");
             }
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                user.Passwort = _passwordHasher.HashPassword(user, user.Passwort);
-                this._dbManager.Add(user);
-                int result = await this._dbManager.SaveChangesAsync();
-                if(result == 0)
+                var passwordHasher = new PasswordHasher<User>();
+                user.Passwort = passwordHasher.HashPassword(user, user.Passwort);
+                _dbManager.Add(user);
+                int result = await _dbManager.SaveChangesAsync();
+                if (result == 0)
                 {
                     return View("Message", new Message()
                     {
@@ -86,7 +83,8 @@ namespace _4AHWII_WebProjekt_MasoodFabian.Controllers
                         MessageText = "Sie konnten leider nicht registriert werden!",
                         Solution = "Bitte versuchen Sie es später erneut!"
                     });
-                } else
+                }
+                else
                 {
                     return View("Message", new Message()
                     {
