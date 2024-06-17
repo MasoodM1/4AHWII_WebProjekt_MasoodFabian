ae877fa1abf8bd0f16e84fbff9b40f58b0cdd5d4
Masood <mmuradi@tsn.at>
2024-06-17 10:12:10 +0200

    added JavaScript

---
+++ 4AHWII_WebProjekt_MasoodFabian/wwwroot/js/JavaScript.js
@@ -0,0 +1,49 @@
+﻿$(() => {
+    let formOk = true;
+
+    $("#Username").on("blur", () => {
+        let username = $("#Username").val();
+        if (checkUsername(username)) {
+            $("#errorUsername").html("");
+        } else {
+            $("#errorUsername").html("Der Benutzername muss <b>mind. 3 Zeichen</b> lang sein");
+            formOk = false;
+        }
+    });
+
+    $("#Passwort").on("blur", () => {
+        let password = $("#Passwort").val();
+        if (checkPassword(password)) {
+            $("#errorPasswort").html("");
+        } else {
+            $("#errorPasswort").html("Das Passwort muss <b>mind. 8 Zeichen</b> lang sein");
+            formOk = false;
+        }
+    });
+
+    $("#btnSubmit").on("click", (event) => {
+        $("#Username").trigger("blur");
+        $("#Passwort").trigger("blur");
+
+        if (!formOk) {
+            event.preventDefault();
+            formOk = true;
+        }
+    });
+});
+
+function checkUsername(username) {
+    username = username.trim();
+    if (username.length < 3) {
+        return false;
+    }
+    return true;
+}
+
+function checkPassword(password) {
+    password = password.trim();
+    if (password.length < 8) {
+        return false;
+    }
+    return true;
+}
